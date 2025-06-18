ba32e3f2060777dc43db1c8d29a01f92454e4e8d
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-18 16:18:42 +0900

    state

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,9 +3,11 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Button from "./components/Button";
+import { useState } from "react";
 
 //부모 컨포넌트
 function App() {
+  //components
   // return (
   //   <>
   //     <Header />
@@ -13,21 +15,48 @@ function App() {
   //     <Footer />
   //   </>
   // );
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  //props
+  // const buttonProps = {
+  //   text: "메일",
+  //   color: "red",
+  //   a: 1,
+  //   b: 2,
+  //   c: 3,
+  // };
+  // return (
+  //   <>
+  //     <Button {...buttonProps} />
+  //     <Button text={"카페"} />
+  //     <Button text={"블로그"}>
+  //       <div>안녕하세요</div>
+  //     </Button>
+  //   </>
+  // );
+
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+  // console.log(count);
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>안녕하세요</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          전구 {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
     </>
   );
 }
