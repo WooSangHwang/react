2df480f3a2507b8c651b61110c3c467343a29288
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-18 15:34:50 +0900

    이벤트 처리

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,18 @@
 const Button = ({ children, text, color = "black" }) => {
   console.log(text);
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button
+      //   onClick={() => {
+      //     console.log(text);
+      //   }}
+      onClick={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
