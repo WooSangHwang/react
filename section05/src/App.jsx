56e4f2307e7df1bcb82dbe24bcca27c5e16e5990
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-18 14:31:50 +0900

    react commpnents

---
+++ section05/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+//부모 컨포넌트
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+//자식 컴퍼넌트
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+export default Header;
