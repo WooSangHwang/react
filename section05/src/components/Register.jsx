8730ef0cfc0839b0efc56e9eefb2e893faeb67d5
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-19 10:40:10 +0900

    code refactoring

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -6,40 +6,67 @@ import { useState } from "react";
 // 4. 자기소개
 
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  // const [name, setName] = useState("이름");
+  // const [birth, setBirth] = useState("");
+  // const [country, setCountry] = useState("");
+  // const [bio, setBio] = useState("");
 
-  const onChangeName = (e) => {
-    // console.log(e.target.value);
-    setName(e.target.value);
-  };
+  const [input, setInput] = useState({
+      name : "",
+      birth:"",
+      country:"",
+      bio:""
+  });
+    console.log(input);
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
-
-  const onChangeCountry = (e) => {
-    console.log(e.target.value);
-    setCountry(e.target.value);
-  };
-
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+    // console.log(input);
+    const onChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name] : e.target.value
+        })
+    }
+  //
+  // const onChangeName = (e) => {
+  //   // console.log(e.target.value);
+  //   // setName(e.target.value);
+  //   setInput({
+  //       ...input,
+  //       name : e.target.value
+  //   })
+  // };
+  //
+  // const onChangeBirth = (e) => {
+  //     setInput({
+  //         ...input,
+  //         birth : e.target.value
+  //     })
+  // };
+  //
+  // const onChangeCountry = (e) => {
+  //     setInput({
+  //         ...input,
+  //         country : e.target.value
+  //     })
+  // };
+  //
+  // const onChangeBio = (e) => {
+  //     setInput({
+  //         ...input,
+  //         bio : e.target.value
+  //     })
+  // };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeHolder={"이름"} />
+        <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
       </div>
       <div>
-        <input value={birth} type="date" onChange={onChangeBirth} />
-        {birth}
+        <input name="birth" value={input.birth} type="date" onChange={onChange} />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -47,8 +74,7 @@ const Register = () => {
         </select>
       </div>
       <div>
-        <textarea onChange={onChangeBio} value={bio} />
-        {bio}
+        <textarea name="bio" onChange={onChange} value={input.bio} />
       </div>
     </div>
   );
