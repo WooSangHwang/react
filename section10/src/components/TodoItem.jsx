12c86f98d63102a11fc5d168225a193d5c0230ca
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-21 16:53:37 +0900

    최적화

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -23,16 +23,19 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
 };
 
 //고차 컴포넌트 (higher order component) HOC
-export default memo(TodoItem, (prevProps, nextProps) => {
-  //반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
-  // T-> Props 바뀌지 않음 -> 리렌더링 x
-  // F-> Props 바뀜 -> 리렌더링 o
-  // console.log(prevProps);
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   //반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+//   // T-> Props 바뀌지 않음 -> 리렌더링 x
+//   // F-> Props 바뀜 -> 리렌더링 o
+//   // console.log(prevProps);
+//
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+//
+//   return true;
+// });
 
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
-
-  return true;
-});
+//useCallback 으로 최적화 처리 함
+export default memo(TodoItem);
