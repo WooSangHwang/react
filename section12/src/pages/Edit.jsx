8f53367f97403b7c4ec238f69584240b34682c75
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-23 16:45:48 +0900

    배포준비

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -5,12 +5,15 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { DiaryStateContext, DiaryDispatchContext } from "../App.jsx";
 import useDiary from "../hooks/useDiary";
+import usePageTitle from "../hooks/usePageTitle.jsx";
 
 const Edit = () => {
   const nav = useNavigate();
   const params = useParams();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
   const curDiaryItem = useDiary(params.id);
+  usePageTitle(`${params.id}번 일기 수정`);
+
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
       onDelete(params.id);
